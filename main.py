c969b03e0df5dbfd9c5b16bc7fb00b0961b9b0bd
Ben <149434798+cshem@users.noreply.github.com>
2023-10-30 19:00:33 -0600

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-"""
+import os
+import pygame
+pygame.init()
 
-?
+path = 'x' # !
+lib = os.listdir(path)
 
-"""
+def main():
+
+    print(f'{len(lib)} files found in ({path})')
+    for i in range(len(lib)):
+        print(f'{i + 1} | {lib[i]}')
+
+    track_idx = int(input('hahahahaha ')) - 1
+    track = pygame.mixer.Sound(path + "//" + lib[track_idx])
+
+    track.play()
+
+
+main()
