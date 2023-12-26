504d49fad9313c23e335ca2786ad916d430e61a6
Ben <bencshem@gmail.com>
2023-12-26 02:55:28 -0700

    2

--- main.py
+++ main.py
@@ -2,19 +2,40 @@ import os
 import pygame
 pygame.init()
 
-path = 'x' # !
-lib = os.listdir(path)
-
 def main():
+    menu()
+
+
+def menu():
+
+    path = r'C:\Users\Ben\Desktop\test' # !
+    lib = os.listdir(path)
+    track_name = []
 
     print(f'{len(lib)} files found in ({path})')
     for i in range(len(lib)):
         print(f'{i + 1} | {lib[i]}')
 
-    track_idx = int(input('hahahahaha ')) - 1
+    track_idx = int(input('select a track id to play: ')) - 1
     track = pygame.mixer.Sound(path + "//" + lib[track_idx])
+    print(f'{lib[i]} selected')
+    playing(track)
 
-    track.play()
+
+def playing(track, track):
+
+    playing = False
+
+    while True:
+        i = input("input: ")
+        if i == 'menu':
+            menu()
+        if i == 'start' and playing == False:
+            track.play()
+            playing = True
+        if i == 'stop':
+            track.stop()
+            playing = False
 
 
 main()
